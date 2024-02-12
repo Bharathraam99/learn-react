18643d0113bfb4f90e1952f2bec3f82a4346c87a
Bharath Raam <bharathsrini19@gmail.com>
2024-02-12 15:29:36 -0500

    Feb 8 Activity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
-
+import styles from "@/styles/Home.module.css";
+import Profile from "./components/profile";
+import Profile2 from "./qcomps/profile_mistake";
+import MyComp from "./qcomps/firstcomp";
+import Bio from "./qcomps/bios";
+import TodoList from "./qcomps/todos";
+import PackingList from "./qcomps/props_item";
+import List from "./qcomps/list_keys_id";
+import RecipeList from "./qcomps/recipes";
+import Gallery from "./qcomps/gallery_props";
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+      <Profile />
+      <Profile2 />
+      <MyComp />
+      <Bio />
+      <TodoList />
+      <Gallery/>
+      <PackingList />
+      <List />
+      <RecipeList />
     </div>
-  )
+  );
 }
--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,55 +1,51 @@
 export default function Gallery() {
+  function Profile({ sName, sPicture, sProfession, sAwards, sDiscovered }) {
+    return (
+      <>
+        <section className="profile">
+          <h2>{sName}</h2>
+          <img
+            className="avatar"
+            src={sPicture}
+            alt={sName}
+            width={70}
+            height={70}
+          />
+          <ul>
+            <li>
+              <b>Profession: </b>
+              {sProfession}
+            </li>
+            <li>
+              <b>Awards: 4 </b>({sAwards})
+            </li>
+            <li>
+              <b>Discovered: </b>
+              {sDiscovered}
+            </li>
+          </ul>
+        </section>
+      </>
+    );
+  }
+
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+      <Profile
+        sName="Maria Skłodowska-Curie"
+        sPicture="https://i.imgur.com/szV5sdGs.jpg"
+        sProfession="physicist and chemist"
+        sAwards="Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal"
+        sDiscovered="polonium (element)"
+      />
+      <Profile
+        sName="Katsuko Saruhashi"
+        sPicture="https://i.imgur.com/YfeOqp2s.jpg"
+        sProfession="geochemist"
+        sAwards="Miyake Prize for geochemistry, Tanaka Prize"
+        sDiscovered="a method for measuring carbon dioxide in seawater"
+      />
     </div>
   );
 }
