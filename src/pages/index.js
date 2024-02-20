01fb8175528a8039bafbc34624644b8a988d1052
Bharath Raam <bharathsrini19@gmail.com>
2024-02-19 19:23:31 -0500

    Feb 15 Activity | Bharath

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import styles from "@/styles/Home.module.css";
+import Profile from "./components/profile";
+import Gallery from "./qcomps/state";
+import Form from "./qcomps/stuckForm";
+import FeedbackForm from "./qcomps/thankYouCrash";
+import RequestTracker from "./qcomps/shoppingCart";
+import Scoreboard from "./qcomps/updObjectsForm";
+import List from "./qcomps/artistsRemoveArr";
+import BucketList from "./qcomps/arrObj";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+      <Profile />
+      <Gallery />
+      <Form />
+      <FeedbackForm />
+      <RequestTracker />
+      <Scoreboard />
+      <List/>
+      <BucketList/>
     </div>
-  )
+  );
 }
