78d8ad18fe4c0ef6c0593d797cb043cd1ceef99f
Bharath Raam <bharathsrini19@gmail.com>
2024-02-21 17:13:15 -0500

    Feb 20 Activity | Bharath

--- src/pages/qcomps/artistsRemoveArr.js
+++ src/pages/qcomps/artistsRemoveArr.js
@@ -1,26 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 let initialArtists = [
-  { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
 ];
 
 export default function List() {
-  const [artists, setArtists] = useState(
-    initialArtists
-  );
-
+  const [artists, setArtists] = useState(initialArtists);
+  const handleDeleteClick = (id) => {
+    const updatedArtists = artists.filter((artist) => artist.id !== id);
+    setArtists(updatedArtists);
+  };
   return (
     <>
       <h1>Inspiring sculptors:</h1>
       <ul>
-        {artists.map(artist => (
+        {artists.map((artist) => (
           <li key={artist.id}>
-            {artist.name}{' '}
-            <button onClick={() => {
-              artists.splice(artist.id, 1)
-            }}>
+            {artist.name}{" "}
+            <button
+              onClick={() => {
+                handleDeleteClick(artist.id);
+              }}
+            >
               Delete
             </button>
           </li>
--- src/pages/qcomps/updObjectsForm.js
+++ src/pages/qcomps/updObjectsForm.js
@@ -1,51 +1,47 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Scoreboard() {
   const [player, setPlayer] = useState({
-    firstName: 'John Woodrow',
-    lastName: 'Wilson',
+    firstName: "John Woodrow",
+    lastName: "Wilson",
     likescore: 10,
   });
 
   function handlePlusClick() {
-    player.likescore++;
+    setPlayer((prevPlayer) => ({
+      ...prevPlayer,
+      likescore: prevPlayer.likescore + 1,
+    }));
   }
 
   function handleFirstNameChange(e) {
-    setPlayer({
-      ...player,
+    setPlayer((prevPlayer) => ({
+      ...prevPlayer,
       firstName: e.target.value,
-    });
+    }));
   }
 
   function handleLastNameChange(e) {
-    setPlayer({
-      lastName: e.target.value
-    });
+    setPlayer((prevPlayer) => ({
+      ...prevPlayer,
+      lastName: e.target.value,
+    }));
   }
 
   return (
     <>
       <label>
         Like Score: <b>{player.likescore}</b>
-        {'  '}
-        <button onClick={handlePlusClick}>
-          +1
-        </button>
+        {"  "}
+        <button onClick={handlePlusClick}>+1</button>
       </label>
       <label>
         First name:
-        <input
-          value={player.firstName}
-          onChange={handleFirstNameChange}
-        />
+        <input value={player.firstName} onChange={handleFirstNameChange} />
       </label>
       <label>
         Last name:
-        <input
-          value={player.lastName}
-          onChange={handleLastNameChange}
-        />
+        <input value={player.lastName} onChange={handleLastNameChange} />
       </label>
     </>
   );
